8e18304393b9f158a4da18b250a41d1625dbb13f
Asım Tahir <a.karakus@pa.ag>
2022-02-04 21:13:33 +0300

    chore(example/vite/vanilla-ts): example init for `vite/vanilla-ts`

--- examples/vite/vanilla-ts/src/main.ts
+++ examples/vite/vanilla-ts/src/main.ts
@@ -3,37 +3,46 @@ import "loadelayed/css/spinner.css";
 
 import "@/assets/style.pcss";
 
-import loadelayed from "loadelayed";
+import Loadelayed from "loadelayed";
 
-const demo1 = document.querySelector(".demo1") as HTMLElement;
-const demo2 = document.querySelector(".demo2") as HTMLElement;
-const demo3 = document.querySelector(".demo3") as HTMLElement;
+Loadelayed.setDefaultShowOptions({
+  full: false,
+  spinner: {
+    position: "center",
+  },
+});
 
 // DEMO 1
-setTimeout(() => {
-  loadelayed.show(demo1);
-  setInterval(() => {
-    loadelayed.hide(demo1);
+const loadelayedDemo1 = new Loadelayed(
+  document.querySelector<HTMLElement>(".demo1")
+);
+setInterval(() => {
+  loadelayedDemo1.show({ full: true, spinner: { position: "top" } });
+  setTimeout(() => {
+    loadelayedDemo1.hide();
   }, 2000);
-}, 3000);
+}, 6000);
 
 // DEMO 2
-setTimeout(() => {
-  loadelayed.show(demo2, {
-    full: true,
-    spinnerPosition: "top",
-  });
-  setInterval(() => {
-    loadelayed.hide(demo2);
-  }, 2000);
-}, 4000);
+const loadelayedDemo2 = new Loadelayed(
+  document.querySelector(".demo2") as HTMLElement
+);
+loadelayedDemo2.show({ spinner: { position: "left" } });
+setInterval(() => {
+  loadelayedDemo2.show({ spinner: { position: "left" } });
+  setTimeout(() => {
+    loadelayedDemo2.hide();
+  }, 2500);
+}, 3500);
 
 // DEMO 3
-setTimeout(() => {
-  loadelayed.show(demo3, {
-    hideContent: false,
-  });
-  setInterval(() => {
-    loadelayed.hide(demo2);
-  }, 2000);
-}, 5000);
+const loadelayedDemo3 = new Loadelayed(
+  document.querySelector(".demo3") as HTMLElement
+);
+loadelayedDemo3.show({ spinner: { position: "bottom" } });
+setInterval(() => {
+  loadelayedDemo3.show({ spinner: { position: "bottom" } });
+  setTimeout(() => {
+    loadelayedDemo3.hide();
+  }, 3000);
+}, 4000);
